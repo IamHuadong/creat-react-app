a3a7a47895b1530bc588fadb2b3ea0e1cc26aa66
huadong.chen <huadong.chen@shopee.com>
2020-12-07 16:03:21 +0800

    feat: update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.jsx',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: '/\.jsx$/',
+        use: '@babel/preset-react'
+      }
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
   }
 };
